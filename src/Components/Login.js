5c2dc69c5e3ae45b90a13fb794d0588ca6a12e1c
Keerati Chuatanapinyo <keeratipond1203@gmail.com>
2020-12-31 21:40:57 +0700

    correct login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,7 +2,7 @@ import React, { Component, useState } from 'react'
 import Styles from './Login.module.css'
 import frogy from '../Images/frogy.jpg'
 import checkLogin from '../Manipulation/LoginManipulation'
-import styled from 'styled-components'
+// import styled from 'styled-components'
 
 function Login(){
     const [account, setAccount] = useState({username: '',
@@ -18,8 +18,8 @@ function Login(){
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        var resString = checkLogin(account.username, account.password) ? 'Welcome!': 'Please try again';
-        alert(resString);
+        // var resString = checkLogin(account.username, account.password) ? 'Welcome!': 'Please try again';
+        // alert(resString);
 
         // if(account.username=='POND' || account.username=='TEAM') alert('Welcome!')
         // else{
@@ -36,10 +36,16 @@ function Login(){
                 <div className={Styles.form}>
                     <form onSubmit={handleSubmit}>
                         <label for='username'>Username</label>
-                        <input type='text' id='username' onChange={handleUsername}/>
+                        <input type='text' 
+                               id='username' 
+                               value={account.username}
+                               onChange={handleUsername}/>
                         <br/>
                         <label for='password'>Password</label>
-                        <input type='password' id='password' onChange={handlePassword}/>
+                        <input type='password' 
+                               id='password' 
+                               value={account.password}
+                               onChange={handlePassword}/>
                         <br/>
                         <div className={Styles.submitbutton}>
                             <input className={Styles.btn} type="submit" value="Login" />
